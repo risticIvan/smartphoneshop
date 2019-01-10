d81d8e5c0ea88be1932765bc8a8f511fa39b4e74
ivan_ristic <ivan_ristic@outlook.com>
2019-01-10 13:10:40 +0100

    Done: Models and data anottations TO do: Entity Framework, Migrations, Interfaces, Repository

---
+++ SmartphoneShop/SmartphoneShop/Models/Shop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartphoneShop.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Adress { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string City { get; set; }
+        [Required]
+        [Range(10000,99999)]
+        public int Postcode { get; set; }
+        [Required]
+        [Phone]
+        public string Telephone { get; set; }
+        [Required]
+        [EmailAddress]
+        public int Email { get; set; }
+    }
+}
---
+++ SmartphoneShop/SmartphoneShop/Models/Smartphone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartphoneShop.Models
+{
+    public class Smartphone
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Brand { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Model { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string OS { get; set; }
+        [Required]
+        [Range(0.1,8)]
+        public decimal ScreenDiagonal { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Chipset { get; set; }
+        [Required]
+        [Range(0.5,32)]
+        public decimal Ram { get; set; }
+        [Required]
+        [Range(1,50)]
+        public int FrontCamera { get; set; }
+        [Required]
+        [Range(1,50)]
+        public int BackCamera { get; set; }
+        [Required]
+        [Range(1, 8000)]
+        public int BatteryCapacity {get; set; }
+        [Required]
+        [Range(1, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
